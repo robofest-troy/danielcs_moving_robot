702719b5f4b65e2983ddd1d6ce0aea87dfbc473d
daniel chen <danielchen6788@gmail.com>
2025-08-10 19:28:30 -0400

    Update setup.py

--- daniel's_robot/src/urdf_robot/setup.py
+++ daniel's_robot/src/urdf_robot/setup.py
@@ -18,7 +18,7 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='daniel',
+    maintainer='danniel',
     maintainer_email='danielchen6788@gmail.com',
     description='TODO: Package description',
     license='Apache-2.0',
